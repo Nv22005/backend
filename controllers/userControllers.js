bdc34b74eaa289b5f70c2db29d03cce0dfa54759
Nv22005 <155470674+Nv22005@users.noreply.github.com>
2024-04-24 10:23:36 +0000

    18 - fixing bugs

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,39 +1,84 @@
 import { uploadPicture } from "../middleware/uploadPictureMiddleware";
 import User from "../models/User";
 import { fileRemover } from "../utils/fileRemover";
-
 const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
-
-	@@ -32,7 +34,7 @@ export const registerUser = async (req, res, next) => {
+    // check whether the user exists or not
+    let user = await User.findOne({ email });
+    if (user) {
+      throw new Error("User have already registered");
+    }
+    // creating a new user
+    user = await User.create({
+      name,
+      email,
+      password,
+    });
+    return res.status(201).json({
+      _id: user._id,
+      avatar: user.avatar,
+      name: user.name,
+      email: user.email,
+      verified: user.verified,
+      admin: user.admin,
+      token: await user.generateJWT(),
+    });
+  } catch (error) {
+    next(error);
   }
 };
-
 const loginUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-
-	@@ -60,7 +62,7 @@ export const loginUser = async (req, res, next) => {
+    let user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("Email not found");
+    }
+    if (await user.comparePassword(password)) {
+      return res.status(201).json({
+        _id: user._id,
+        avatar: user.avatar,
+        name: user.name,
+        email: user.email,
+        verified: user.verified,
+        admin: user.admin,
+        token: await user.generateJWT(),
+      });
+    } else {
+      throw new Error("Invalid email or password");
+    }
+  } catch (error) {
+    next(error);
   }
 };
-
 const userProfile = async (req, res, next) => {
   try {
     let user = await User.findById(req.user._id);
-
-	@@ -83,4 +85,93 @@ export const userProfile = async (req, res, next) => {
+    if (user) {
+      return res.status(201).json({
+        _id: user._id,
+        avatar: user.avatar,
+        name: user.name,
+        email: user.email,
+        verified: user.verified,
+        admin: user.admin,
+      });
+    } else {
+      let error = new Error("User not found");
+      error.statusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
   }
 };
-
 const updateProfile = async (req, res, next) => {
   try {
     let user = await User.findById(req.user._id);
-
     if (!user) {
       throw new Error("User not found");
     }
-
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
     if (req.body.password && req.body.password.length < 6) {
@@ -41,9 +86,7 @@ const updateProfile = async (req, res, next) => {
     } else if (req.body.password) {
       user.password = req.body.password;
     }
-
     const updatedUserProfile = await user.save();
-
     res.json({
       _id: updatedUserProfile._id,
       avatar: updatedUserProfile.avatar,
@@ -57,25 +100,27 @@ const updateProfile = async (req, res, next) => {
     next(error);
   }
 };
-
 const updateProfilePicture = async (req, res, next) => {
   try {
     const upload = uploadPicture.single("profilePicture");
 
     upload(req, res, async function (err) {
       if (err) {
-        const error = new Error("An unknown error occured when uploading " + err.message);
+        const error = new Error(
+          "An unknown error occured when uploading " + err.message
+        );
         next(error);
       } else {
         // every thing went well
         if (req.file) {
-          const updatedUser = await User.findByIdAndUpdate(
-            req.user._id,
-            {
-              avatar: req.file.filename,
-            },
-            { new: true }
-          );
+          let filename;
+          let updatedUser = await User.findById(req.user._id);
+          filename = updatedUser.avatar;
+          if (filename) {
+            fileRemover(filename);
+          }
+          updatedUser.avatar = req.file.filename;
+          await updatedUser.save();
           res.json({
             _id: updatedUser._id,
             avatar: updatedUser.avatar,
@@ -108,7 +153,6 @@ const updateProfilePicture = async (req, res, next) => {
     next(error);
   }
 };
-
 export {
   registerUser,
   loginUser,
