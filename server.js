dd3e9eff69276032ba6403c1ed3e3e8647370e66
Nv22005 <155470674+Nv22005@users.noreply.github.com>
2024-04-24 09:53:46 +0000

    13 - Connecting node.js to database

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-import express from "express";   
-
+import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
 
 dotenv.config();
-
-const app =express ();
-app.use(express.json()) 
+connectDB();
+const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Server is running...");
-  });
-
-  const PORT = process.env.PORT || 5000;
-
-  app.listen(5000, () => console.log ("server is running on port ${PORT}"));
+  res.send("Server is running...");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
